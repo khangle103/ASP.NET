a9c5208842f082a4061234a0dfb4c4ce635d46e4
khangle103 <letanxuankhang103@gmail.com>
2024-12-11 16:09:01 +0700

    First update

---
+++ LeTanXuanKhang/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeTanXuanKhang.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult AllCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ LeTanXuanKhang/Controllers/ListingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeTanXuanKhang.Controllers
+{
+    public class ListingController : Controller
+    {
+        // GET: Listing
+        public ActionResult Grid()
+        {
+            return View();
+        }
+        public ActionResult Large()
+        {
+            return View();
+        }
+    }
+}
